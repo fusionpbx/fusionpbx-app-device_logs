0610702a1611e77d476962a133e3ac8477afc5cd
FusionPBX <markjcrane@gmail.com>
2022-10-12 12:37:56 -0600

    Create device_logs.php

---
+++ device_logs.php
@@ -0,0 +1,272 @@
+<?php
+/*
+	Copyright (c) 2019-2022 Mark J Crane <markjcrane@fusionpbx.com>
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions
+	are met:
+
+		1. Redistributions of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+	
+		2. Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+	OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+	HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+	LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+	OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+	SUCH DAMAGE.
+*/
+
+//includes
+	require_once "root.php";
+	require_once "resources/require.php";
+	require_once "resources/check_auth.php";
+	require_once "resources/paging.php";
+
+//check permissions
+	if (permission_exists('device_log_view')) {
+		//access granted
+	}
+	else {
+		echo "access denied";
+		exit;
+	}
+
+//add multi-lingual support
+	$language = new text;
+	$text = $language->get();
+
+//get the http post data
+	if (is_array($_POST['device_logs'])) {
+		$action = $_POST['action'];
+		$search = $_POST['search'];
+		$device_logs = $_POST['device_logs'];
+	}
+
+//process the http post data by action
+	if ($action != '' && is_array($device_logs) && @sizeof($device_logs) != 0) {
+		switch ($action) {
+			case 'copy':
+				if (permission_exists('device_log_add')) {
+					$obj = new device_logs;
+					$obj->copy($device_logs);
+				}
+				break;
+			case 'toggle':
+				if (permission_exists('device_log_edit')) {
+					$obj = new device_logs;
+					$obj->toggle($device_logs);
+				}
+				break;
+			case 'delete':
+				if (permission_exists('device_log_delete')) {
+					$obj = new device_logs;
+					$obj->delete($device_logs);
+				}
+				break;
+		}
+
+		header('Location: device_logs.php'.($search != '' ? '?search='.urlencode($search) : null));
+		exit;
+	}
+
+//get order and order by
+	$order_by = $_GET["order_by"];
+	$order = $_GET["order"];
+
+//add the search string
+	if (isset($_GET["search"])) {
+		$search =  strtolower($_GET["search"]);
+		$sql_search = " (";
+		$sql_search .= "	lower(device_mac_address) like :search ";
+		$sql_search .= "	or lower(request_scheme) like :search ";
+		$sql_search .= "	or lower(http_host) like :search ";
+		$sql_search .= "	or lower(server_port) like :search ";
+		$sql_search .= "	or lower(server_protocol) like :search ";
+		$sql_search .= "	or lower(query_string) like :search ";
+		$sql_search .= "	or lower(remote_address) like :search ";
+		$sql_search .= "	or lower(http_user_agent) like :search ";
+		$sql_search .= "	or lower(http_status) like :search ";
+		$sql_search .= "	or lower(http_status_code) like :search ";
+		$sql_search .= ") ";
+		$parameters['search'] = '%'.$search.'%';
+	}
+
+//get the count
+	$sql = "select count(device_log_uuid) from v_device_logs ";
+	if ($_GET['show'] == "all" && permission_exists('device_log_all')) {
+		if (isset($sql_search)) {
+			$sql .= "where ".$sql_search;
+		}
+	}
+	else {
+		$sql .= "where (domain_uuid = :domain_uuid or domain_uuid is null) ";
+		if (isset($sql_search)) {
+			$sql .= "and ".$sql_search;
+		}
+		$parameters['domain_uuid'] = $domain_uuid;
+	}
+	$database = new database;
+	$num_rows = $database->select($sql, $parameters, 'column');
+
+//prepare to page the results
+	$rows_per_page = ($_SESSION['domain']['paging']['numeric'] != '') ? $_SESSION['domain']['paging']['numeric'] : 50;
+	$param = $search ? "&search=".$search : null;
+	$param = ($_GET['show'] == 'all' && permission_exists('device_log_all')) ? "&show=all" : null;
+	$page = is_numeric($_GET['page']) ? $_GET['page'] : 0;
+	list($paging_controls, $rows_per_page) = paging($num_rows, $param, $rows_per_page);
+	list($paging_controls_mini, $rows_per_page) = paging($num_rows, $param, $rows_per_page, true);
+	$offset = $rows_per_page * $page;
+
+//get the list
+	$sql = str_replace('count(device_log_uuid)', '*', $sql);
+	$sql .= order_by($order_by, $order, 'timestamp', 'desc');
+	$sql .= limit_offset($rows_per_page, $offset);
+	$database = new database;
+	$device_logs = $database->select($sql, $parameters, 'all');
+	unset($sql, $parameters);
+
+//create token
+	$object = new token;
+	$token = $object->create($_SERVER['PHP_SELF']);
+
+//include the header
+	$document['title'] = $text['title-device_logs'];
+	require_once "resources/header.php";
+
+//show the content
+	echo "<div class='action_bar' id='action_bar'>\n";
+	echo "	<div class='heading'><b>".$text['title-device_logs']." (".$num_rows.")</b></div>\n";
+	echo "	<div class='actions'>\n";
+	if (permission_exists('device_log_add')) {
+		echo button::create(['type'=>'button','label'=>$text['button-add'],'icon'=>$_SESSION['theme']['button_icon_add'],'id'=>'btn_add','link'=>'device_log_edit.php']);
+	}
+	if (permission_exists('device_log_add') && $device_logs) {
+		echo button::create(['type'=>'button','label'=>$text['button-copy'],'icon'=>$_SESSION['theme']['button_icon_copy'],'name'=>'btn_copy','onclick'=>"modal_open('modal-copy','btn_copy');"]);
+	}
+	if (permission_exists('device_log_edit') && $device_logs) {
+		echo button::create(['type'=>'button','label'=>$text['button-toggle'],'icon'=>$_SESSION['theme']['button_icon_toggle'],'name'=>'btn_toggle','onclick'=>"modal_open('modal-toggle','btn_toggle');"]);
+	}
+	if (permission_exists('device_log_delete') && $device_logs) {
+		echo button::create(['type'=>'button','label'=>$text['button-delete'],'icon'=>$_SESSION['theme']['button_icon_delete'],'name'=>'btn_delete','onclick'=>"modal_open('modal-delete','btn_delete');"]);
+	}
+	echo 		"<form id='form_search' class='inline' method='get'>\n";
+	if (permission_exists('device_log_all')) {
+		if ($_GET['show'] == 'all') {
+			echo "		<input type='hidden' name='show' value='all'>\n";
+		}
+		else {
+			echo button::create(['type'=>'button','label'=>$text['button-show_all'],'icon'=>$_SESSION['theme']['button_icon_all'],'link'=>'?show=all']);
+		}
+	}
+	echo 		"<input type='text' class='txt list-search' name='search' id='search' value=\"".escape($search)."\" placeholder=\"".$text['label-search']."\" onkeydown='list_search_reset();'>";
+	echo button::create(['label'=>$text['button-search'],'icon'=>$_SESSION['theme']['button_icon_search'],'type'=>'submit','id'=>'btn_search','style'=>($search != '' ? 'display: none;' : null)]);
+	echo button::create(['label'=>$text['button-reset'],'icon'=>$_SESSION['theme']['button_icon_reset'],'type'=>'button','id'=>'btn_reset','link'=>'device_logs.php','style'=>($search == '' ? 'display: none;' : null)]);
+	if ($paging_controls_mini != '') {
+		echo 	"<span style='margin-left: 15px;'>".$paging_controls_mini."</span>\n";
+	}
+	echo "		</form>\n";
+	echo "	</div>\n";
+	echo "	<div style='clear: both;'></div>\n";
+	echo "</div>\n";
+
+	if (permission_exists('device_log_add') && $device_logs) {
+		echo modal::create(['id'=>'modal-copy','type'=>'copy','actions'=>button::create(['type'=>'button','label'=>$text['button-continue'],'icon'=>'check','id'=>'btn_copy','style'=>'float: right; margin-left: 15px;','collapse'=>'never','onclick'=>"modal_close(); list_action_set('copy'); list_form_submit('form_list');"])]);
+	}
+	if (permission_exists('device_log_edit') && $device_logs) {
+		echo modal::create(['id'=>'modal-toggle','type'=>'toggle','actions'=>button::create(['type'=>'button','label'=>$text['button-continue'],'icon'=>'check','id'=>'btn_toggle','style'=>'float: right; margin-left: 15px;','collapse'=>'never','onclick'=>"modal_close(); list_action_set('toggle'); list_form_submit('form_list');"])]);
+	}
+	if (permission_exists('device_log_delete') && $device_logs) {
+		echo modal::create(['id'=>'modal-delete','type'=>'delete','actions'=>button::create(['type'=>'button','label'=>$text['button-continue'],'icon'=>'check','id'=>'btn_delete','style'=>'float: right; margin-left: 15px;','collapse'=>'never','onclick'=>"modal_close(); list_action_set('delete'); list_form_submit('form_list');"])]);
+	}
+
+	echo $text['description-device_logs']."\n";
+	echo "<br /><br />\n";
+
+	echo "<form id='form_list' method='post'>\n";
+	echo "<input type='hidden' id='action' name='action' value=''>\n";
+	echo "<input type='hidden' name='search' value=\"".escape($search)."\">\n";
+
+	echo "<table class='list'>\n";
+	echo "<tr class='list-header'>\n";
+	if (permission_exists('device_log_add') || permission_exists('device_log_edit') || permission_exists('device_log_delete')) {
+		echo "	<th class='checkbox'>\n";
+		echo "		<input type='checkbox' id='checkbox_all' name='checkbox_all' onclick='list_all_toggle();' ".($device_logs ?: "style='visibility: hidden;'").">\n";
+		echo "	</th>\n";
+	}
+	if ($_GET['show'] == 'all' && permission_exists('device_log_all')) {
+		echo th_order_by('domain_name', $text['label-domain'], $order_by, $order);
+	}
+	echo th_order_by('timestamp', $text['label-timestamp'], $order_by, $order);
+	echo th_order_by('device_mac_address', $text['label-device_mac_address'], $order_by, $order);
+	echo th_order_by('request_scheme', $text['label-request_scheme'], $order_by, $order);
+	echo th_order_by('http_host', $text['label-http_host'], $order_by, $order);
+	echo th_order_by('server_port', $text['label-server_port'], $order_by, $order);
+	echo th_order_by('server_protocol', $text['label-server_protocol'], $order_by, $order);
+	echo th_order_by('query_string', $text['label-query_string'], $order_by, $order);
+	echo th_order_by('remote_address', $text['label-remote_address'], $order_by, $order);
+	echo th_order_by('http_user_agent', $text['label-http_user_agent'], $order_by, $order);
+	echo th_order_by('http_status', $text['label-http_status'], $order_by, $order);
+	echo th_order_by('http_status_code', $text['label-http_status_code'], $order_by, $order);
+	if (permission_exists('device_log_edit') && $_SESSION['theme']['list_row_edit_button']['boolean'] == 'true') {
+		echo "	<td class='action-button'>&nbsp;</td>\n";
+	}
+	echo "</tr>\n";
+
+	if (is_array($device_logs) && @sizeof($device_logs) != 0) {
+		$x = 0;
+		foreach ($device_logs as $row) {
+			if (permission_exists('device_log_edit')) {
+				$list_row_url = "device_log_edit.php?id=".urlencode($row['device_log_uuid']);
+			}
+			echo "<tr class='list-row' href='".$list_row_url."'>\n";
+			if (permission_exists('device_log_add') || permission_exists('device_log_edit') || permission_exists('device_log_delete')) {
+				echo "	<td class='checkbox'>\n";
+				echo "		<input type='checkbox' name='device_logs[$x][checked]' id='checkbox_".$x."' value='true' onclick=\"if (!this.checked) { document.getElementById('checkbox_all').checked = false; }\">\n";
+				echo "		<input type='hidden' name='device_logs[$x][uuid]' value='".escape($row['device_log_uuid'])."' />\n";
+				echo "	</td>\n";
+			}
+			if ($_GET['show'] == 'all' && permission_exists('device_log_all')) {
+				echo "	<td>".escape($_SESSION['domains'][$row['domain_uuid']]['domain_name'])."</td>\n";
+			}
+			echo "	<td>".escape($row['timestamp'])."</td>\n";
+			echo "	<td>".escape($row['device_mac_address'])."</td>\n";
+			echo "	<td>".escape($row['request_scheme'])."</td>\n";
+			echo "	<td>".escape($row['http_host'])."</td>\n";
+			echo "	<td>".escape($row['server_port'])."</td>\n";
+			echo "	<td>".escape($row['server_protocol'])."</td>\n";
+			echo "	<td>".escape($row['query_string'])."</td>\n";
+			echo "	<td>".escape($row['remote_address'])."</td>\n";
+			echo "	<td>".escape($row['http_user_agent'])."</td>\n";
+			echo "	<td>".escape($row['http_status'])."</td>\n";
+			echo "	<td>".escape($row['http_status_code'])."</td>\n";
+			if (permission_exists('device_log_edit') && $_SESSION['theme']['list_row_edit_button']['boolean'] == 'true') {
+				echo "	<td class='action-button'>\n";
+				echo button::create(['type'=>'button','title'=>$text['button-edit'],'icon'=>$_SESSION['theme']['button_icon_edit'],'link'=>$list_row_url]);
+				echo "	</td>\n";
+			}
+			echo "</tr>\n";
+			$x++;
+		}
+		unset($device_logs);
+	}
+
+	echo "</table>\n";
+	echo "<br />\n";
+	echo "<div align='center'>".$paging_controls."</div>\n";
+	echo "<input type='hidden' name='".$token['name']."' value='".$token['hash']."'>\n";
+	echo "</form>\n";
+
+//include the footer
+	require_once "resources/footer.php";
+
+?>
