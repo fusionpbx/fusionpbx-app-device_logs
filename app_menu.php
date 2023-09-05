9c7450764022e868cee5c47bfdff0321bb37b0cd
Anthony <96078404+AntoniusDC@users.noreply.github.com>
2023-09-05 16:37:34 -0600

    Update Device Logs app_menu.php (#6)

--- app_menu.php
+++ app_menu.php
@@ -1,13 +1,28 @@
 <?php
 
 $apps[$x]['menu'][0]['title']['en-us'] = 'Device Logs';
-$apps[$x]['menu'][0]['title']['es-cl'] = '';
-$apps[$x]['menu'][0]['title']['fr-fr'] = '';
-$apps[$x]['menu'][0]['title']['fr-ca'] = '';
-$apps[$x]['menu'][0]['title']['pl'] = '';
-$apps[$x]['menu'][0]['title']['sv-se'] = '';
-$apps[$x]['menu'][0]['title']['uk'] = '';
-$apps[$x]['menu'][0]['title']['de-at'] = '';
+$apps[$x]['menu'][0]['title']['en-gb'] = 'Device Logs';
+$apps[$x]['menu'][0]['title']['ar-eg'] = 'سجلات الجهاز';
+$apps[$x]['menu'][0]['title']['de-at'] = 'Geräteprotokolle';
+$apps[$x]['menu'][0]['title']['de-ch'] = 'Geräteprotokolle';
+$apps[$x]['menu'][0]['title']['de-de'] = 'Geräteprotokolle';
+$apps[$x]['menu'][0]['title']['es-cl'] = 'Registros de dispositivos';
+$apps[$x]['menu'][0]['title']['es-mx'] = 'Registros de dispositivos';
+$apps[$x]['menu'][0]['title']['fr-ca'] = 'Journaux de périphérique';
+$apps[$x]['menu'][0]['title']['fr-fr'] = 'Journaux de périphérique';
+$apps[$x]['menu'][0]['title']['he-il'] = 'יומני מכשיר';
+$apps[$x]['menu'][0]['title']['it-it'] = 'Registri del dispositivo';
+$apps[$x]['menu'][0]['title']['nl-nl'] = 'Apparaatlogboeken';
+$apps[$x]['menu'][0]['title']['pl-pl'] = 'Dzienniki urządzenia';
+$apps[$x]['menu'][0]['title']['pt-br'] = 'Registros de dispositivos';
+$apps[$x]['menu'][0]['title']['pt-pt'] = 'Registros de dispositivos';
+$apps[$x]['menu'][0]['title']['ro-ro'] = 'Jurnalele dispozitivului';
+$apps[$x]['menu'][0]['title']['ru-ru'] = 'Журналы устройств';
+$apps[$x]['menu'][0]['title']['uk-ua'] = 'Журнали пристроїв';
+$apps[$x]['menu'][0]['title']['sv-se'] = 'Enhetsloggar';
+$apps[$x]['menu'][0]['title']['zh-cn'] = '设备日志';
+$apps[$x]['menu'][0]['title']['ja-jp'] = 'デバイスログ';
+$apps[$x]['menu'][0]['title']['ko-kr'] = '장치 로그';
 $apps[$x]['menu'][0]['uuid'] = 'b81d58ca-3e2a-4c2c-b876-dcae5edcb38b';
 $apps[$x]['menu'][0]['parent_uuid'] = '0438b504-8613-7887-c420-c837ffb20cb1';
 $apps[$x]['menu'][0]['category'] = 'internal';
