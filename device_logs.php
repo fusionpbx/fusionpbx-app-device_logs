dde88de8eb3e3c4d50bc3e82f7fd92c137a3b601
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-09 10:03:37 -0700

    Update device_logs.php

--- device_logs.php
+++ device_logs.php
@@ -246,6 +246,7 @@
 	echo "<input type='hidden' id='action' name='action' value=''>\n";
 	echo "<input type='hidden' name='search' value=\"".escape($search)."\">\n";
 
+	echo "<div class='card'>\n";
 	echo "<table class='list'>\n";
 	echo "<tr class='list-header'>\n";
 	if (permission_exists('device_log_add') || permission_exists('device_log_edit') || permission_exists('device_log_delete')) {
@@ -312,6 +313,7 @@
 	}
 
 	echo "</table>\n";
+	echo "</div>\n";
 	echo "<br />\n";
 	echo "<div align='center'>".$paging_controls."</div>\n";
 	echo "<input type='hidden' name='".$token['name']."' value='".$token['hash']."'>\n";
